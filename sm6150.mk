ece0a5c4e13c29859469f64a3cf4206ae3082d94
Musafir <askibrahim06@gmail.com>
2025-12-19 21:45:04 +0530

    sm6150-common: add ViPER4AndroidFX support

--- sm6150.mk
+++ sm6150.mk
@@ -32,6 +32,9 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# ViPER4AndroidFX
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
 # AID/fs configs
 PRODUCT_PACKAGES += \
     fs_config_files
