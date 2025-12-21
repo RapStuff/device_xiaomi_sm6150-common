923ee7a4086e422dc483c473fe353881b30698bd
Musafir02 <askibrahim06@gmail.com>
2025-12-21 07:30:21 +0000

    add support for sony dolby vision

--- sm6150.mk
+++ sm6150.mk
@@ -35,6 +35,9 @@ PRODUCT_COPY_FILES += \
 # ViPER4AndroidFX
 $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 
+# Sony Dolby
+$(call inherit-product, hardware/dolby/dolby.mk)
+
 # AID/fs configs
 PRODUCT_PACKAGES += \
     fs_config_files
